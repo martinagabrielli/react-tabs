2044000791e85034404e83675c5229230a890c37
martinagabrielli <martina.gabrielli@yahoo.it>
2021-01-17 18:40:54 +0000

    added useState and useEffect

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import './App.css';
+import React, { useState, useEffect } from 'react';
 import { FaAngleDoubleRight } from 'react-icons/fa';
+import './App.css';
 const url = 'https://course-api.com/react-tabs-project';
 
 function App() {
+  const [loading, setLoading] = useState();
   return (
     <div className="App">
       Hello
